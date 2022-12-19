49288214777620ac666a0534a9926c038ab76680
AMStudioAM <abdullahgomah2000@gmail.com>
2022-12-20 00:10:31 +0300

    remove dashboard application

--- dashboard/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import * 
-
-app_name= 'dashboard' 
-
-urlpatterns = [
-    path('', dashboard, name='dashboard'), 
-    path('getlastfive', get_last_five_mosannaf, name='get-lastfive'),
-]
--- project/settings.py
+++ project/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'printinghouse',
     'publisher',
     'mosannaf',
-    'dashboard', 
 
 ]
 
